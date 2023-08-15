432cbd19df8d21d7878961a188887eeb9a4aca9c
Ayush <ayushmanisharma@gmail.com>
2023-08-15 12:20:51 -0400

    Added abilty to modify user data

--- src/routes/api/userRoutes.js
+++ src/routes/api/userRoutes.js
@@ -1,6 +1,36 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
-const User = require('../../models/User');
+const { User, Score } = require('../../models');
+
+// GET all users
+router.get('/', async (req, res) => {
+    try {
+      const userData = await User.findAll({
+        include: [{ model: Score }],
+      });
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  // GET a single user
+router.get('/:id', async (req, res) => {
+    try {
+      const userData = await User.findByPk(req.params.id, {
+        include: [{ model: Score }],
+      });
+  
+      if (!userData) {
+        res.status(404).json({ message: 'No user found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 // CREATE a new user
 router.post('/', async (req, res) => {
@@ -15,5 +45,25 @@ router.post('/', async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+// DELETE a user
+router.delete('/:id', async (req, res) => {
+    try {
+      const userData = await User.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!userData) {
+        res.status(404).json({ message: 'No user found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
